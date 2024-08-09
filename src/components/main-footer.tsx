0910d4b8c510b74c16aab1595f1c082de0cdf387
Bruno Lamps <lamparolho@gmail.com>
2024-08-09 12:38:42 -0300

    learning tailwind, formatting page.tsx

---
+++ src/components/main-footer.tsx
@@ -0,0 +1,9 @@
+export default async function MainFooter() {
+    return (
+        <footer className="z-10 w-full h-12 fixed bottom-0 flex flex-row justify-between items-center px-4 bg-yellow-300">
+            <div className="profileMenu">profile menu</div>
+            <div className="menuOptions">menu options</div>
+            <div className="logo">logo</div>
+        </footer>
+    )
+}
--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -2,7 +2,7 @@ import type { NextAuthConfig } from "next-auth";
 
 export default {
   secret: process.env.AUTH_SECRET,
-  basePath: "/api/auth",
+  //basePath: "/api/auth",
   providers: [],
   pages: {
     // signIn: "/signin",
